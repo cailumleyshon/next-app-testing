226b430c464c6e7637b97de923b64c656f6e8272
Cailum Leyshon <107961224+cailumleyshon@users.noreply.github.com>
2023-11-23 09:26:44 +0000

    Init commit

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This is my root route</h1>
+      <h1>This is the blogs page, cool!</h1>
     </main>
   );
 }
